9cdf53953fe68a65724eb0a4eb37bbb001314301
Chanchico <juanfranciscotraversi@gmail.com>
2023-04-02 10:40:40 +0200

    texture

--- sdl/src/main.c
+++ sdl/src/main.c
@@ -5,48 +5,63 @@
 	SDL_RENDERER_ACCELERATE
 	SDL_RENDERER_PRESENTVSYNC
 	SDL_RENDERER_TARGETTEXTURE
+	Voir les differents commits pour les examples 
 */
 
 int main(int argc, char **argv){
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
-	
+
 	///lancement SDL
 	if(SDL_Init(SDL_INIT_VIDEO) != 0)
 		SDL_ExitWithError("Init SDL");
 
 	//Création de fenetre et rendu
-	if(SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer) != 0)
+	if(SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer) != 0)
 		SDL_ExitWithError("Unable to create the windows and the render");
 	
 
 	//Execution du programme
 	/*_____________________________________________________________*/
-	if(SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE) != 0)
-		SDL_ExitWithError("Unable to change the render color");
-	
-	if(SDL_RenderDrawPoint(renderer, 155, 455) != 0)
-		SDL_ExitWithError("Unable to print the point");
+	SDL_Surface *image = NULL;
+	SDL_Texture *texture = NULL;
 
-	if(SDL_RenderDrawLine(renderer, 50, 50, 50, 300))
-		SDL_ExitWithError("Unable to print the line");
+	image = SDL_LoadBMP("src/sample.bmp");
+	if(image == NULL)
+		{
+			SDL_ClearMemory(window, renderer);
+			SDL_ExitWithError("Unable to load the image");
+		}
+
+	texture = SDL_CreateTextureFromSurface(renderer, image);
+	SDL_FreeSurface(image); //On libere la mémoire !!
+
+	if(texture == NULL){
+		SDL_ClearMemory(window, renderer);
+		SDL_ExitWithError("Unable to create the texture");
+	}
 
 	SDL_Rect rectangle;
-	rectangle.x = 300;
-	rectangle.y = 300;
-	rectangle.w = 100;
-	rectangle.h = 50;
 
-	if(SDL_RenderDrawRect(renderer, &rectangle) != 0)
-		SDL_ExitWithError("Unable to print the rectangle");
+	if (SDL_QueryTexture(texture, NULL, NULL, &rectangle.w, &rectangle.h) != 0){
+		SDL_ClearMemory(window, renderer);
+		SDL_ExitWithError("Unable to create the texture");
+	}
+	
+	rectangle.x = (WINDOW_WIDTH - rectangle.w) / 2;
+	rectangle.y = (WINDOW_HEIGHT - rectangle.h) /2;
+
+	if(SDL_RenderCopy(renderer, texture, NULL, &rectangle) != 0){
+		SDL_ClearMemory(window, renderer);
+		SDL_ExitWithError("Unable to display the texture");
+	}
 
 	SDL_RenderPresent(renderer);
 	SDL_Delay(3000);
 	/*_____________________________________________________________*/
 
-	//ON PENSE A DETRUIRE TOUT RISQUE DE FUITE MEMOIRE
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
+	//PENSER A DETRUIRE TOUT RISQUE DE FUITE MEMOIRE
+	SDL_ClearMemory(window, renderer);
 	SDL_Quit();
 	return EXIT_SUCCESS;
 }
@@ -55,4 +70,9 @@ void SDL_ExitWithError(const char *message){
 	SDL_Log("Error : %s > %s \n", message, SDL_GetError());
 	SDL_Quit();
 	exit(EXIT_FAILURE);
+}
+
+void SDL_ClearMemory(SDL_Window *window, SDL_Renderer *renderer ){
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 }
--- sdl/src/main.h
+++ sdl/src/main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN
-# define MAIN
+#define MAIN
 
-	#include <stdio.h>
-	#include <SDL.h>
-	#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+#include <stdlib.h>
 
-	void SDL_ExitWithError(const char *message);
+void SDL_ExitWithError(const char *message);
+void SDL_ClearMemory(SDL_Window *window, SDL_Renderer *renderer);
 
+#endif
 
-# endif
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 426
